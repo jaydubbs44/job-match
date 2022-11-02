366afda3bd138111e15c0b2a16cbb980da10a8fa
Will Christen <christenwill58@gmail.com>
2022-11-02 13:15:02 -0400

    Initial Commit with Hello World Functionality

---
+++ src/main/java/org/launchcode/jobmatch/JobMatchApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.jobmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/jobmatch/JobMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.jobmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
