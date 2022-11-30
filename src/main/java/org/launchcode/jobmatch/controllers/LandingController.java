7776cde898b6bd71b246658ec08a9ab43ee4a106
Will Christen <christenwill58@gmail.com>
2022-11-30 15:13:51 -0500

    Initial Authentication Filter Setup. Debugging Needed

--- src/main/java/org/launchcode/jobmatch/controllers/LandingController.java
+++ src/main/java/org/launchcode/jobmatch/controllers/LandingController.java
@@ -2,11 +2,12 @@ package org.launchcode.jobmatch.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LandingController {
 
-    @GetMapping
+    @GetMapping("")
     public String showLandingPage(){
         return "landing";
     }
