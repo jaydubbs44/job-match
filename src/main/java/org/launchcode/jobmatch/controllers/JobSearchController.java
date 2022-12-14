1bd73bc01878c1464af8f181bb7dca568a92c3ee
Paulina Ponce <paulina.poncemarcano@gmail.com>
2022-12-14 15:30:51 -0500

    implemented indeed api to job search page

--- src/main/java/org/launchcode/jobmatch/controllers/JobSearchController.java
+++ src/main/java/org/launchcode/jobmatch/controllers/JobSearchController.java
@@ -12,8 +12,7 @@ public class JobSearchController {
 
     @GetMapping("jobsearch")
     public String index (Model model) {return "jobsearch";}
-
-       public HttpResponse<String> response = Unirest.post("https://indeed11.p.rapidapi.com/")
+    public HttpResponse<String> response = Unirest.post("https://indeed11.p.rapidapi.com/")
                 .header("content-type", "application/json")
                 .header("X-RapidAPI-Key", "c1d1d45673mshb872bacdff5241dp1373fdjsn45adf7f84af6")
                 .header("X-RapidAPI-Host", "indeed11.p.rapidapi.com")
